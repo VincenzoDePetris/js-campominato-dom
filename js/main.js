fcff81d77c4fb32a2f3b96b18ac54a0ebff1e848
VincenzoDePetris <vince02@live.it>
2023-08-23 10:49:39 +0200

    add: bombs bg

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const button = document.getElementById("button");
 const griglia = document.getElementById("main");
 
+let bombe;
+let score;
+let cellsFreeTotal;
+
 button.addEventListener("click", function () {
   griglia.innerHTML = "";
   generaGriglia();
@@ -13,21 +17,22 @@ function getRandomNumber(min, max) {
   return newRandomNumber;
 }
 
-function generaGriglia() {
-  function checkNumber(array, element) {
-    let trovato = false;
+// funzione per comparare
 
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] == element) {
-        trovato = true;
-      }
+function checkNumber(array, element) {
+  let trovato = false;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == element) {
+      trovato = true;
     }
-    return trovato;
   }
+  return trovato;
+}
 
+function generaGriglia() {
   //creazione bombe
-
-  let bombe = [];
+  const bombe = [];
 
   while (bombe.length < 16) {
     let newNumber = getRandomNumber(1, 100);
@@ -40,17 +45,37 @@ function generaGriglia() {
 
   console.log(bombe);
 
+  // inizializzazione totale celle libere
+
+  let score = 0;
+
+  let cellsFreeTotal = 100 - bombe.length;
+
   //creazione celle
+
   for (let i = 1; i <= 100; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
+
     // solo per ora metto la classe hard per motivi di formattazione
+
     cell.classList.add("hard");
     cell.innerHTML = i;
 
+    // al click della cella
+
     cell.addEventListener("click", function pippo() {
-      cell.classList.add("color");
-      console.log("cella numero " + i);
+      //cell.classList.add("color");
+      //console.log("cella numero " + i);
+
+      const cellNumber = i;
+      if (bombe.includes(cellNumber)) {
+        cell.classList.add("lose");
+        alert("fine partita");
+      } else {
+        cell.classList.add("color");
+        score++;
+      }
     });
 
     griglia.append(cell);
